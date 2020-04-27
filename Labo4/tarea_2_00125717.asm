a6b5a569301396f1ae3314e290118ee212f0242d
Elsy Chavez <00125717@gmail.com>
2020-04-26 20:36:05 -0600

    Mensaje del ejercicio 1, finalizado

--- Labo4/tarea_2_00125717.asm
+++ Labo4/tarea_2_00125717.asm
@@ -4,6 +4,7 @@
 
         ;Carnet = 00125717, digitos a ocupar 25717
         ; (2+5+7+1+7)/5 = 22/5 = 4.4
+        ;Mensaje: me recupero :,v
 
         mov ax, 0000h
         add ax, 2d
@@ -13,6 +14,28 @@
         add ax, 7d
         mov cl, 5d
         div cl
+
+        mov cl, "M"
+        mov [200h], cl
+        mov cl, "E"
+        mov [201h], cl
+
+        mov cl, "R"
+        mov [203h], cl
+        mov cl, "E"
+        mov [204h], cl
+        mov cl, "C"
+        mov [205h], cl
+        mov cl, "U"
+        mov [206h], cl
+        mov cl, "P"
+        mov [207h], cl
+        mov cl, "E"
+        mov [208h], cl
+        mov cl, "R"
+        mov [209h], cl
+        mov cl, "O"
+        mov [20Ah], cl
         
         ;linea final
         int 20h

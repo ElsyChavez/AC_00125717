0101ba7eec6ba5c51c3dd8d7f796e10721917284
Elsy Chavez <00125717@gmail.com>
2020-04-26 20:29:28 -0600

    Primera parte ejercicio 1

--- Labo4/tarea_2_00125717.asm
+++ Labo4/tarea_2_00125717.asm
@@ -2,5 +2,17 @@
 
         org 100h;
 
+        ;Carnet = 00125717, digitos a ocupar 25717
+        ; (2+5+7+1+7)/5 = 22/5 = 4.4
+
+        mov ax, 0000h
+        add ax, 2d
+        add ax, 5d
+        add ax, 7d
+        add ax, 1d
+        add ax, 7d
+        mov cl, 5d
+        div cl
         
-        
+        ;linea final
+        int 20h
